79d3570ed801fdce6b5ad605eac970d322cd0f08
wddwxy <wxy2008@yeah.net>
2013-12-21 02:18:51 +0800

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ FirstApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root to: 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
